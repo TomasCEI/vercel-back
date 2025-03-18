8d61574c871e78d9311be4e9570c45497a768a85
TomasCEI <147064032+TomasCEI@users.noreply.github.com>
2025-03-18 18:53:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); // procesa el json body para leer con req.body()
+app.use(express.urlencoded({extended:false})); // leer datos de urlEncoded de req.body()
+
+
+// Nos permite acceder a los archivos de nuestro backend
+app.use('/uploads', express.static('public/uploads'));
+app.use('/web', express.static('public/'));
+
+// Rutas
+app.get("/", (req, res)=> {
+    res.setHeader("Content-Type", "text/html");
+
+    const nombre = process.env.NOMBRE || "Mundo";
+    const landingHTML = `
+        <h1>Bienvenidos a nuestra Web de Express!</h1>
+        Variables de entorno: ${nombre}
+    `;
+    res.send(landingHTML);
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    }
+);
