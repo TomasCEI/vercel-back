9d2b7df27465a0ea1aa2690ff3e9946e515a4b88
TomichArg <tomich20@gmail.com>
2025-03-20 14:14:16 +0100

    update vercel.json

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/uploads', express.static('public/uploads'));
 
 // para usar esto mejor trabajar con VITE + Express
 // https://vercel.community/t/express-101-everything-about-deploying-your-express-app-on-vercel/4870
-//app.use('/web', express.static('public/web'));
+app.use('/web', express.static('public/web'));
 
 // Rutas
 app.get("/", (req, res) => {
